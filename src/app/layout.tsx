ce1327271d3a2588ca47296f5f7e23de8226fee4
Mabel Castillo <maby.garza@gmail.com>
2024-11-14 19:13:05 -0800

    add netlify identity widget

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
