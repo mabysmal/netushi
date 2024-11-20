8e618ae2dcb90006c1cba044c44f91050168e2db
Mabel Castillo <maby.garza@gmail.com>
2024-11-19 18:21:59 -0800

    next.config.js changed for ext images y standalone para netlify

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  output: 'standalone',
 };
 
-export default nextConfig;
+module.exports = nextConfig;
