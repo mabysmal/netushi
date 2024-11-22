19b924922f6d9ddf6868cec1fbb299302a7f3978
Mabel Castillo <maby.garza@gmail.com>
2024-11-22 14:27:21 -0800

    bussiness hours antes de merge con decap

--- src/app/componentes/BusinessHoursSection.tsx
+++ src/app/componentes/BusinessHoursSection.tsx
@@ -4,13 +4,7 @@ import BusinessHours from "./BusinessHours";
 const BusinessHoursSection = () => {
   // Definimos los horarios del negocio
   const businessHours = {
-    'Lunes': { open: '12:00', close: '22:00' },
-    'Martes': { open: '12:00', close: '22:00' },
-    'Miércoles': { open: '12:00', close: '22:00' },
-    'Jueves': { open: '12:00', close: '22:00' },
-    'Viernes': { open: '12:00', close: '23:00' },
-    'Sábado': { open: '12:00', close: '23:00' },
-    'Domingo': { open: '12:00', close: '22:00' }
+    //Aqui deben ir los horarios que se agregaron en el panel de admin
   };
 
   return (
